fc318e93b53e71aa0a6bb0da55270ea6d5ada77d
edwko <187129830+edwko@users.noreply.github.com>
2024-11-04 18:01:35 +0200

    Update alignment.py

--- outetts/v0_1/alignment.py
+++ outetts/v0_1/alignment.py
@@ -1,3 +1,34 @@
+# https://github.com/pytorch/audio
+
+# BSD 2-Clause License
+
+# Copyright (c) 2017 Facebook Inc. (Soumith Chintala), 
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Implimentation of CTC forced alignment
+# https://pytorch.org/audio/stable/tutorials/ctc_forced_alignment_api_tutorial.html
+
 import torchaudio
 import torch
 import torchaudio.functional as F
@@ -5,8 +36,6 @@ import inflect
 import re
 
 class CTCForcedAlignment:
-    # Implimentation of CTC forced alignment
-    # https://pytorch.org/audio/stable/tutorials/ctc_forced_alignment_api_tutorial.html
 
     def __init__(self):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
