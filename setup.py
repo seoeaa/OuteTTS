e8614bc4772538ee34cb5e4dedab3349b377d02f
edwko <187129830+edwko@users.noreply.github.com>
2024-11-05 20:32:38 +0200

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r', encoding='utf-8') as fh:
 
 setup(
     name='outetts',
-    version='0.1.5',
+    version='0.1.6',
     packages=find_packages(),
     install_requires=install_requires,
     author='OuteAI',
